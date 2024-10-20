d06ef4c49babf944caeeafea80b75767a14205eb
karolkkqg <zs22013661@estudiantes.uv.mx>
2024-10-20 02:11:46 -0600

    inicializando proyecto

---
+++ src/main/java/com/mycompany/shm/SHM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shm;
+
+/**
+ *
+ * @author kahun
+ */
+public class SHM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
