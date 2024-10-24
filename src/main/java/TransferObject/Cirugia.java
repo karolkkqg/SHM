972ed8a70cef38c7d8d7c370cbc4c199ac01866f
Emmanuel Pale Molina <epalemolina@gmail.com>
2024-10-24 09:12:20 -0600

    Transfer object Cirugia, Enfermedad y Vacuna

--- src/main/java/TransferObject/Cirugia.java
+++ src/main/java/TransferObject/Cirugia.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TransferObject;
+
+import java.sql.Date;
+import java.util.Objects;
 
 /**
  *
  * @author epale
  */
 public class Cirugia {
+    private int id;
+    private String nombre;
+    private Date fechaAplicacion;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaAplicacion(Date fechaAplicacion) {
+        this.fechaAplicacion = fechaAplicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Date getFechaAplicacion() {
+        return fechaAplicacion;
+    }
     
+    @Override
+    public boolean equals(Object obj) {
+        boolean esIgual;
+        
+        if (obj == null || getClass() != obj.getClass()) {
+            esIgual = false;
+        }
+        else {
+            final Cirugia other = (Cirugia) obj;
+            esIgual = this.id == other.id && this.nombre.equals(other.nombre) && this.fechaAplicacion == other.fechaAplicacion;
+        }
+
+        return esIgual;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.nombre);
+        hash = 59 * hash + Objects.hashCode(this.fechaAplicacion);
+        return hash;
+    }
 }
--- src/main/java/TransferObject/Enfermedad.java
+++ src/main/java/TransferObject/Enfermedad.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TransferObject;
+
+import java.util.Objects;
 
 /**
  *
  * @author epale
  */
 public class Enfermedad {
+    private int id;
+    private String nombre;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
     
+    @Override
+    public boolean equals(Object obj) {
+        boolean esIgual;
+        
+        if (obj == null || getClass() != obj.getClass()) {
+            esIgual = false;
+        }
+        else {
+            final Enfermedad other = (Enfermedad) obj;
+            esIgual = this.id == other.id && this.nombre.equals(other.nombre);
+        }
+
+        return esIgual;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.nombre);
+        return hash;
+    }
 }
