4daa66255666253ea1c3ef9c461e851b3d6c50c5
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-10-26 00:17:57 -0600

    Clases de dominio

---
+++ src/main/java/TransferObject/Paciente.java
@@ -0,0 +1,93 @@
+package TransferObject;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author luis_
+ */
+public class Paciente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private Date fechaNacimiento;
+    private String correo;
+    private String contrasena;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return id == paciente.id
+                && Objects.equals(nombre, paciente.nombre)
+                && Objects.equals(apellido, paciente.apellido)
+                && Objects.equals(genero, paciente.genero)
+                && Objects.equals(fechaNacimiento, paciente.fechaNacimiento)
+                && Objects.equals(correo, paciente.correo)
+                && Objects.equals(contrasena, paciente.contrasena);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, apellido, genero, fechaNacimiento, correo, contrasena);
+    }
+}
